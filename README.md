aa0531a042ef2f3a896f4387ab3dac30f88c6ff2
Ethan Lowman <ethan.l.527@gmail.com>
2015-06-16 10:52:02 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# SublimeEmoji
+
+A simple Sublime Text 3 plugin to insert emoji from the command palette.
+
+![Screenshot](http://i.imgur.com/DqU3h8u.png)
---
+++ SublimeEmoji.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+import sublime
+import sublime_plugin
+import os
+import codecs
+import re
+
+
+def load_emoji():
+    path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "emoji-data.txt")
+    f = codecs.open(path, encoding="utf-8")
+    data = []
+    names = {}
+    for line in f:
+        if line[0] == "#":
+            continue
+        code, emoji, name = re.findall("^([A-Z0-9 ]+) ;.+\((.+)\) (.+)", line)[0]
+        names[code] = name
+        name = name.title()
+        data.append([code, emoji, name, "{} {} ({})".format(emoji, name, code)])
+
+    return sorted(data, key=lambda x: names[x[0]])
+
+
+class PasteEmojiCommand(sublime_plugin.TextCommand):
+
+    def run(self, edit, emoji):
+        sublime.status_message("ran")
+        for sel in self.view.sel():
+            self.view.insert(edit, sel.begin(), emoji)
+
+
+class EmojiCommand(sublime_plugin.TextCommand):
+
+    def run(self, edit):
+        window = sublime.active_window()
+        data = load_emoji()
+        items = [t[-1] for t in data]
+
+        def callback(selection):
+            emoji = data[selection][1]
+            self.view.run_command("paste_emoji", {"emoji": emoji})
+
+        window.show_quick_panel(items, callback)
